bdacfe97d33ebdecfd6c8261a1f21b129c1e7d2d
杉村篤哉 <>
2022-11-18 13:27:05 +0900

    localhostで動くことを確認

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 // 省略
 
+
+
+const FILE_URL = "./photos/0010171846705174-web.jpg";
+const MODEL_URL = "./weights";
+
+let img, canvas, context;
+
+window.onload = (event) => {
+    console.log("onload!");
+    loadModels();
+}
+
+async function loadModels() {
+    console.log("loadModels");
+    Promise.all([
+        faceapi.loadSsdMobilenetv1Model(MODEL_URL),
+        faceapi.loadFaceLandmarkModel(MODEL_URL),
+        faceapi.loadFaceRecognitionModel(MODEL_URL)
+    ]).then(detectAllFaces);
+}
+
+async function detectAllFaces() {
+    console.log("detectAllFaces");
+    // ここで画像解析を行います。
+}
+
+
 async function detectAllFaces() {
     console.log("detectAllFaces");
 
